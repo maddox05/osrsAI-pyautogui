fbafb0a0fb03fb8a2a64c64481af206ab0663fd7
maddox05 <maddoxpublic@gmail.com>
2023-09-18 19:31:32 -0400

    fixed it so i dont have add images it does it for me.

--- assets.py
+++ assets.py
@@ -1,38 +1,27 @@
 import os
 
+tree_1_maple_images = os.listdir("assets/seers-village-maples/tree-1-maple/")
 
-maple_trees_1_folder = "assets/seers-village-maples/tree-1-maple/"
-tree_1_maple_images = os.listdir(maple_trees_1_folder)
+tree_2_maple_images = os.listdir("assets/seers-village-maples/tree-2-maple/")
 
-maple_trees_2_folder = "assets/seers-village-maples/tree-2-maple/"
-tree_2_maple_images = os.listdir(maple_trees_2_folder)
+tree_3_maple_images = os.listdir("assets/seers-village-maples/tree-3-maple/")
 
-maple_trees_3_folder = "assets/seers-village-maples/tree-3-maple/"
-tree_3_maple_images = os.listdir(maple_trees_3_folder)
+tree_4_maple_images = os.listdir("assets/seers-village-maples/tree-4-maple/")
 
-maple_trees_4_folder = "assets/seers-village-maples/tree-4-maple/"
-tree_4_maple_images = os.listdir(maple_trees_4_folder)
+maple_deposit = os.listdir("assets/seers-village-maples/maple-deposit/")
 
-maple_deposit_folder = "assets/seers-village-maples/maple-deposit/"
-maple_deposit = os.listdir(maple_deposit_folder)
+bank_images = os.listdir("assets/seers-village-maples/bank/")
 
-bank_folder = "assets/seers-village-maples/bank/"
-bank_images = os.listdir(bank_folder)
+tree_1_dead_maple = os.listdir("assets/seers-village-maples-dead/tree-1-maple/")
 
-maple_tree_1_dead_folder = "assets/seers-village-maples-dead/tree-1-maple/"
-tree_1_dead_maple = os.listdir(maple_tree_1_dead_folder)
+tree_2_dead_maple = os.listdir("assets/seers-village-maples-dead/tree-2-maple/")
 
-maple_tree_2_dead_folder = "assets/seers-village-maples-dead/tree-2-maple/"
-tree_2_dead_maple = os.listdir(maple_tree_2_dead_folder)
+tree_3_dead_maple = os.listdir("assets/seers-village-maples-dead/tree-3-maple/")
 
-maple_tree_3_dead_folder = "assets/seers-village-maples-dead/tree-3-maple/"
-tree_3_dead_maple = os.listdir(maple_tree_3_dead_folder)
+tree_4_dead_maple = os.listdir("assets/seers-village-maples-dead/tree-4-maple/")
 
-maple_tree_4_dead_folder = "assets/seers-village-maples-dead/tree-4-maple/"
-tree_4_dead_maple = os.listdir(maple_tree_4_dead_folder)
+compass_images = os.listdir("assets/compass/")
 
-compass_folder = "assets/compass/"
-compass_images = os.listdir(compass_folder)
 
 def printAsiccArt():
     print(" __  __              _       _                      __     ___   ")
@@ -40,5 +29,3 @@ def printAsiccArt():
     print("| |\/| | / _` |  / _` |  / _` |   / _ \   \ \ /   | () |  |__ \  ")
     print("|_|__|_| \__,_|  \__,_|  \__,_|   \___/   /_\_\   _\__/   |___/  ")
     print("By Maddox05 (https://github.com/maddox05)")
-
-
